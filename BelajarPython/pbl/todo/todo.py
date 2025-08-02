ac36caee3216bc9ea49865841d48fc7dce7b3f8c
risyady <ibrahimrisyad28@gmail.com>
2025-08-02 19:30:37 +0700

    Make a parser for my apps

--- BelajarPython/pbl/todo/todo.py
+++ BelajarPython/pbl/todo/todo.py
@@ -1,9 +1,29 @@
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="To Do List CLI")
+subparsers = parser.add_subparsers(dest="command")
 
-parser.add_argument("add", type=str, help="Menambahkan task")
-parser.add_argument("list", help="Menampilkan semua task")
-parser.add_argument("done", type=int, help="Menandai task yang sudah selesai")
-parser.add_argument("delete", type=int, help="Menghapus task")
+parser_list = subparsers.add_parser("list", help="Menampilkan semua tugas")
+
+parser_add = subparsers.add_parser("add", help="Menambahkan tugas baru")
+parser_add.add_argument("task", type=str, help="Masukkan nama tugas")
+
+parser_done = subparsers.add_parser("done", help="Mengubah status tugas menjadi selesai")
+parser_done.add_argument("id", type=int, help="Menyelesaikan tugas")
+
+parser_delete = subparsers.add_parser("delete", help="Menghapus tugas")
+parser_delete.add_argument("delete", type=int, help="Menghapus tugas")
+
 args = parser.parse_args()
+
+args.command = "list" if args.command == None else args.command
+
+match args.command:
+    case "list":
+        print("Fungsi List")
+    case "add":
+        print("Fungsi Add")
+    case "done":
+        print("Fungsi Done")
+    case "delete":
+        print("Fungsi Delete")
